6c1e7cd047902128eff232f16ce7fe7cfe86a24d
Dickson <dkerobean@gmail.com>
2023-11-10 09:52:29 +0000

    database connection made

--- retailq/settings.py
+++ retailq/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'retailq.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'retailq',
+        'USER': 'postgres',
+        'PASSWORD': 'frogman28',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
