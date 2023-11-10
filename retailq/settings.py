9f9436ba7a7ef2c94e20de086e1f295d0b8394eb
Dickson <dkerobean@gmail.com>
2023-11-10 11:56:56 +0000

    implemented email login

--- retailq/settings.py
+++ retailq/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'api',
     'rest_framework',
+
+    'api',
     'user'
 ]
 
@@ -129,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
+AUTHENTICATION_BACKENDS = ['user.backends.EmailBackend']
