a6890ee21461224e48820a672c973ce187c2f87e
Dickson <dkerobean@gmail.com>
2023-11-13 18:21:21 +0000

    test auth

---
+++ user/tests/test_auth.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+from user.models import CustomUser
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class UserRegistrationLoginTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.registration_url = reverse('user-register')
+        self.login_url = reverse('token-obtain-pair')
+
+        # sample registration data
+        self.user_data = {
+            'organization_name': 'Test Organization',
+            'email': 'test@example.com',
+            'password': 'test_password',
+            'country': 'test_country',
+        }
+
+        # sample user for testing purposes
+        self.user = CustomUser.objects.create_user(
+            organization_name=self.user_data.get('organization_name'),
+            email=self.user_data.get('email'),
+            password=self.user_data.get('password'),
+            country=self.user_data.get('country')
+        )
+
+    def test_user_registration(self):
+        response = self.client.post(self.registration_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 1)
+
+    def test_user_login(self):
+        response = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('acess', response.data)
+        self.assertIn('acess', response.data)
--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('register/', views.RegisterationView.as_view(), name="user-register"),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # noqa
+    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'), # noqa
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # noqa
 
 
