de20e6edf8afccd173c9f876a12788ce7e228153
Dickson <dkerobean@gmail.com>
2023-11-15 10:03:56 +0000

    added corsheader for react frontend

--- retailq/settings.py
+++ retailq/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders'
 
     'api',
     'user'
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'retailq.urls'
@@ -139,10 +142,12 @@ AUTHENTICATION_BACKENDS = [
     'user.backends.EmailBackend'
     ]
 
-
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+]
