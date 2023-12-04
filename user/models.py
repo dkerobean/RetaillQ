35b18494d85339abe5d4d1caa6d2934a011a40bd
Dickson <dkerobean@gmail.com>
2023-12-04 09:08:58 +0000

    added products model

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import (AbstractBaseUser,
                                         PermissionsMixin,
                                         BaseUserManager)
 import uuid
-from .utils import generate_organization_id
+from .utils import generate_organization_id, generate_product_id
 
 
 class CustomUserManager(BaseUserManager):
@@ -63,6 +63,16 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Products(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product_id = models.IntegerField(default=generate_product_id)
+    name = models.CharField(max_length=75)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Transaction(models.Model):
--- user/utils.py
+++ user/utils.py
@@ -3,3 +3,7 @@ import random
 
 def generate_organization_id():
     return random.randint(10000000, 99999999)
+
+
+def generate_product_id():
+    return random.randint(10000, 99999)
