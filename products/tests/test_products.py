9b41ddd7f16e0379e441a5c6262fa6419f82232b
Dickson <dkerobean@gmail.com>
2023-12-04 10:03:41 +0000

    added CRUD test cases to the product model

---
+++ products/tests/test_products.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from user.models import Products, CustomUser
+
+
+class ProductTestCase(APITestCase):
+    def setUp(self):
+
+        # create a user for testing
+        self.user = CustomUser.objects.create_user(
+            email="testuser@example.com",
+            password="testpassword",
+            organization_name="testorganization",
+            country="testcountry",
+        )
+
+        # authenticate the user
+        self.client.force_authenticate(user=self.user)
+
+        # create a product for testing
+        self.product_data = {
+            'user': self.user.id,
+            'name': 'Test Product',
+            'price': '12.99',
+            'quantity': 1,
+        }
+        self.products = Products.objects.create(**self.product_data)
+
+        self.url = reverse('product', kwargs={'pk': self.product.id})
+
+    def test_create_product(self):
+        new_product_data = {
+            'user': self.user.id,
+            'name': 'new product',
+            'price': '12.99',
+            'quantity': 1,
+        }
+
+        response = self.client.post(reverse('products'), new_product_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Products.objects.count(), 2)
+
+    def test_retrieve_product(self):
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], 'Test Product')
+
+    def test_update_product(self):
+
+        updated_product_data = {
+            'name': 'Updated Product',
+            'quantity': 15,
+            'price': 25.99,
+        }
+
+        response = self.client.put(self.url, updated_product_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.product.refresh_from_db()
+        self.assertEqual(self.product.name, 'Updated Product')
+
+    def test_delete_product(self):
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Products.objects.count(), 0)
