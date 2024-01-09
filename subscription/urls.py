e8fdb389ff8d38ae24cf63565d165b1dd3708d77
Dickson <dkerobean@gmail.com>
2024-01-09 13:06:32 +0000

    subscription upgrage

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('all/', views.SubscriptionView.as_view(), name="all-subscriptions"),
+    path('upgrade/', views.UpgradeSubscriptionView.as_view(), name="upgrade-subscription"),
 ]
 
 
--- subscription/views.py
+++ subscription/views.py
@@ -14,3 +14,29 @@ class SubscriptionView(APIView):
         subscriptions = Subscription.objects.all()
         serializer = SubscriptionSerializer(subscriptions, many=True)
         return Response(serializer.data if subscriptions else [], status=status.HTTP_200_OK)
+
+
+class UpgradeSubscriptionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        new_plan = request.data.get('new_plan')
+
+        # Ensure the new plan is valid
+        if new_plan not in ['standard', 'premium']:
+            return Response({'error': 'Invalid subscription plan'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Fetch the user's profile
+        profile = user.profiles
+
+        # Fetch the selected subscription plan
+        subscription = Subscription.objects.get(plan='premium_monthly')
+
+        # Update the user's subscription
+        profile.subscription = subscription
+        profile.save()
+
+        return Response({'message': 'Subscription upgraded successfully'},
+                        status=status.HTTP_200_OK)
