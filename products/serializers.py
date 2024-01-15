4616900fde0e00d9ed7514b84cfce850219ffca3
Dickson <dkerobean@gmail.com>
2024-01-15 13:49:21 +0000

    bug fix in deliveries list

--- products/serializers.py
+++ products/serializers.py
@@ -13,7 +13,19 @@ class ProductsSerializer(serializers.ModelSerializer):
         extra_kwargs = {'product_id': {'write_only': True}}
 
 
+class ProductListSerializer(serializers.ModelSerializer):
+    currency = serializers.CharField(source='user.profiles.currency_symbol',
+                                     read_only=True)
+
+    class Meta:
+        model = Products
+        fields = ['price', 'remaining_percentage', 'currency']
+
+
 class DeliverySerializer(serializers.ModelSerializer):
+
+    product = ProductListSerializer()
+
     class Meta:
         model = Delivery
         fields = '__all__'
